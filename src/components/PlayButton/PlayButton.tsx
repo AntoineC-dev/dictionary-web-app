0f20ce495c35e891dba910c21c8883fd48dd1469
Antoine Cheminat <ac.dev@hotmail.com>
2023-09-20 15:06:49 -0400

    Create PlayButton component

---
+++ src/components/PlayButton/PlayButton.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import styles from './PlayButton.module.css';
+import { APIData } from '../../api/types';
+
+type PlayButtonProps = Pick<APIData, 'word' | 'phonetic'>;
+
+function PlayButton(props: PlayButtonProps) {
+  const playMp3 = React.useCallback(() => {
+    return new Audio(props.phonetic.audio).play();
+  }, [props.phonetic.audio]);
+  return (
+    <div className={styles.wrapper}>
+      <div>
+        <h2 className={styles.title}>{props.word}</h2>
+        <span className={styles.phonetic}>{props.phonetic.text}</span>
+      </div>
+      <button className={styles.btn} aria-label="Read the audio of the current word" onClick={playMp3}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 75 75">
+          <g fill="#A445ED" fillRule="evenodd">
+            <circle cx="37.5" cy="37.5" r="37.5" opacity=".25" />
+            <path d="M29 27v21l21-10.5z" />
+          </g>
+        </svg>
+      </button>
+    </div>
+  );
+}
+
+export default PlayButton;
