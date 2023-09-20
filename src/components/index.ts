a35c80577668fed71b6e64f025f81403ccea832a
Antoine Cheminat <ac.dev@hotmail.com>
2023-09-20 15:20:24 -0400

    Export all new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
+export { default as ErrorDisplay } from './ErrorDisplay/ErrorDisplay';
 export { default as FontSelectMenu } from './FontSelectMenu/FontSelectMenu';
+export { default as Meaning } from './Meaning/Meaning';
+export { default as PlayButton } from './PlayButton/PlayButton';
 export { default as SearchField } from './SearchField/SearchField';
 export { default as ThemeSwitch } from './ThemeSwitch/ThemeSwitch';
