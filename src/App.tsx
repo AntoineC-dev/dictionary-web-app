b04c23414b8665a3b79a1e2ba0285f7b5260155c
Antoine Cheminat <ac.dev@hotmail.com>
2023-09-20 15:08:07 -0400

    Update App w/ all new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import { SearchField } from './components';
-import { Navbar } from './layouts';
+import { ErrorDisplay, SearchField } from './components';
+import { Navbar, SuccessDisplay } from './layouts';
+import useStore from './stores/store';
 
 function App() {
+  const store = useStore((state) => state.store);
   return (
     <div className="app">
       <Navbar />
       <SearchField />
-      <p style={{ marginTop: '80px', fontSize: '20px' }}>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi aperiam exercitationem aliquid sit dignissimos
-        accusantium ab similique ut quam officia animi a eligendi laborum, corrupti reiciendis minus saepe suscipit
-        voluptatibus! Quibusdam corrupti sint doloremque vel voluptates distinctio omnis nulla maxime numquam
-        dignissimos, assumenda quia odit, atque maiores repellat reprehenderit nobis magni quos officia ratione
-        voluptatem, fuga blanditiis. Accusamus, eveniet pariatur! Ipsum eligendi harum ducimus minus. Enim minus
-        quisquam eveniet at minima laborum, quaerat ad assumenda deserunt cum mollitia omnis nostrum magnam dolores
-        possimus blanditiis sapiente delectus! Odit explicabo consectetur ratione. Beatae enim quibusdam, repellendus
-        delectus voluptates, a non sed corrupti, dolores minima deserunt autem! Aperiam, nam et! Quia harum obcaecati,
-        sequi, incidunt unde saepe atque ratione fugiat eos, quaerat accusamus. Unde, beatae quisquam! Laboriosam itaque
-        labore, eius voluptates minus non esse saepe aliquid? Illum laboriosam placeat, voluptatum libero, aperiam odit
-        incidunt alias soluta sapiente, enim beatae ad possimus sunt consectetur.
-      </p>
+      {store.data && <SuccessDisplay {...store.data} />}
+      {store.error && <ErrorDisplay {...store.error} />}
     </div>
   );
 }
